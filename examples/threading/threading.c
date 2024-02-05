6040414edb01f9a9ce5c41d9ea967b6ada42de5c
Viet Nguyen <mrholao09@gmail.com>
2024-02-05 12:57:17 +0700

    assignment-4-part-1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+#define delayed_ms(time_ms)	{ usleep(time_ms * 1000); }
+
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
 //#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
@@ -10,10 +13,21 @@
 
 void* threadfunc(void* thread_param)
 {
-
-    // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
+    delayed_ms(thread_func_args->wait_to_obtain_ms);
+
+    pthread_mutex_lock(thread_func_args->mutex);
+
+    delayed_ms(thread_func_args->wait_to_release_ms);
+
+    pthread_mutex_unlock(thread_func_args->mutex);
+
+    thread_func_args->thread_complete_success = true;
+
     return thread_param;
 }
 
@@ -28,6 +42,25 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    bool success = false;
+
+    struct thread_data *thread_func_args = (struct thread_data *)malloc(1 * sizeof(struct thread_data));
+    thread_func_args->mutex = mutex;
+    thread_func_args->wait_to_obtain_ms = wait_to_obtain_ms;
+    thread_func_args->wait_to_release_ms = wait_to_release_ms;
+
+    int result = pthread_create(thread, NULL, threadfunc, (void *)thread_func_args);
+
+    if (result != 0) {
+        success = false;
+        thread_func_args->thread = thread;
+    }
+    else{
+        thread_func_args->thread = thread;
+        success = true;
+    }
+    
+    return success;
+    
 }
 
